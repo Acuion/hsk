e23981cbb5cbf0b3517e30aa39995e254a5bfa54
Acuion <natarur@yandex.com>
2017-01-10 16:03:24 +0300

    Стандартизация кавычек, защищённый random

--- engine/leaderboard.php
+++ engine/leaderboard.php
@@ -2,7 +2,7 @@
 	include 'dbworks.php';
 
 	$tojson = array();
-	$query = $mysqli->query("SELECT anon_id, score, killed_count, alive FROM KillerTheGame ORDER BY killed_count DESC");
+	$query = $mysqli->query('SELECT anon_id, score, killed_count, alive FROM KillerTheGame ORDER BY killed_count DESC');
 	while($result = $query->fetch_assoc())
 		$tojson[] = $result;
 	if ($_GET['count'])
--- engine/regvialms.php
+++ engine/regvialms.php
@@ -20,19 +20,19 @@
 		}
 		else
 		{
-			$loginAns = curl_POST("http://lms.hse.ru/index.php?index_page","_qf__login_form=&user_login=$lmsl&user_password=$lmsp&userLogin=%D0%92%D0%BE%D0%B9%D1%82%D0%B8",$member['id']);
+			$loginAns = curl_POST('http://lms.hse.ru/index.php?index_page','_qf__login_form=&user_login=$lmsl&user_password=$lmsp&userLogin=%D0%92%D0%BE%D0%B9%D1%82%D0%B8',$member['id']);
 			$toProc = curl_GET('http://lms.hse.ru/student.php?ctg=personal&op=account', $member['id']);
 			if (strpos($toProc, 'Группа пользователей') !== false)
 			{
 				$dep = MySubstring($toProc, 'Группа пользователей', true);
 				$dep = MySubstring(MySubstring($dep, 'value="', true), '"', false);
-				if ($dep === 'Н НН БИиПМ 15 ПИ' || $dep === 'Н НН БИиПМ 15 ПМИ')
+				if ($dep === 'Н НН БИиПМ 15 ПИ' || $dep === 'Н НН БИиПМ 15 ПМИ')//TODO
 				{
 					$anontaken = ($mysqli->query("SELECT * FROM KillerTheGame WHERE anon_id = '$anonid'"));
 					$anontaken = $anontaken->num_rows;
 					if ($anontaken === 0)
 					{
-						$sword = ($mysqli->query("SELECT Word FROM Words WHERE Used = 0 ORDER BY Rand()"));
+						$sword = ($mysqli->query('SELECT Word FROM Words WHERE Used = 0 ORDER BY Rand()'));
 						$sword = $sword->fetch_assoc();
 						$sword = $sword['Word'];
 						$mysqli->query("UPDATE Words SET Used = 1 WHERE Word = '".$sword."'");
@@ -40,8 +40,7 @@
 						$alpG = array('у','е','ы','а','о','э','я','и','ю');
 						$alpS = array('й','ц','к','н','ш','щ','з','х','ф','в','п','р','л','д','ж','ч','с','м','т','б');
 						
-						srand(floor(time() * 0.73));
-						$dword = $alpG[rand(0,8)].$alpS[rand(0,19)].$alpG[rand(0,8)].$alpS[rand(0,19)].$alpG[rand(0,8)];
+						$dword = $alpG[random_int(0,8)].$alpS[random_int(0,19)].$alpG[random_int(0,8)].$alpS[random_int(0,19)].$alpG[random_int(0,8)];
 						
 						$fname = MySubstring(MySubstring($toProc, 'name="surname" type="text" value="', true), '"', false);
 						$sname = MySubstring(MySubstring($toProc, 'name="name" type="text" value="', true), '"', false);
