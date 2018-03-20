8feb4b1097b55baf69e36d4f50160284c2b7025e
Yuriy <natarur@yandex.com>
2018-03-20 21:18:29 +0300

    Game vis

--- index.py
+++ index.py
@@ -1,6 +1,18 @@
 from hsespionage import app
+from hsespionage import pgInstance
 from flask import render_template
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/gamevis")
+def gamevis():
+    labels = [] # id:
+    edges = [] # from: to:
+    players = pgInstance().all("SELECT * FROM players", back_as=dict)
+    for player in players:
+        labels.append({'id': player['vk_id']})
+        for vic in player['victims_ids']:
+            edges.append({'from': player['vk_id'], 'to': vic})
+    return render_template("vis.html", labels=labels, edges=edges)
