7553f3ddad7c1c417fa07f0af73e0c2f47b923a5
Yuriy <natarur@yandex.com>
2018-03-22 21:23:25 +0300

    New ordering rule

--- leaderboard.py
+++ leaderboard.py
@@ -6,6 +6,6 @@ from flask import request
 @app.route("/engine/leaderboard")
 def leaderboard():
     leaderboard_data = pgInstance().all(
-        "SELECT anon_id, score, killed_count, alive FROM players ORDER BY score DESC",
+        "SELECT anon_id, score, killed_count, alive FROM players ORDER BY score DESC, last_kill_time ASC, anon_id ASC",
         back_as=dict)
     return json.dumps(leaderboard_data)
